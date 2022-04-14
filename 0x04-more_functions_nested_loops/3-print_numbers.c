fd028c9290a9334da788eee2110cee9e5cc12f87
jude adesulu <judeadesuluflyer6@gmail.com>
2022-04-14 14:21:38 -0700

    Print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Prints 0 to 9
+ *
+ * Return: The numbers 0 - 9
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
