39f3a5d94ecfdf20a0892ee6235515a6181de70a
jude adesulu <judeadesuluflyer6@gmail.com>
2022-04-20 16:40:39 -0700

    Return the length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ *
+ *@s: Pointer with the address value
+ *
+ *Return - value
+ */
+
+
+
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*s != '\0')
+{
+i++;
+s++;
+}
+return (i);
+}
