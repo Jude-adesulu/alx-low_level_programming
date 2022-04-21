7d14270ab8ff88fd09e12aedea871cd2e4790293
jude adesulu <judeadesuluflyer6@gmail.com>
2022-04-20 17:04:31 -0700

    Print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - main function.
+ * @str: Pointer, the string to print.
+ *
+ * Return: none
+ */
+
+void puts2(char *str)
+{
+int i;
+
+while (str[i])
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+
+i++;
+}
+_putchar('\n');
+}
